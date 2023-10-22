3a18284881c24102b066e68035e5b498b5a7acd0
Luis Jover <luisjoverdev@gmail.com>
2023-10-22 12:15:38 +0200

    style(loginpage): responsive modifications

--- src/pages/loginPage/LoginPage.tsx
+++ src/pages/loginPage/LoginPage.tsx
@@ -7,14 +7,16 @@ export const LoginPage = () => {
 
     return (
 
-        <section>
+        <section className='login-page-section'>
             <div className='login-page-img-container'>
                 <img className='login-page-img' src={BgImg} alt={`Image or Cover of Ovni`} />
                 <div className='login-page-glass'>
                 </div>
                 <div className='login-page-title'>
-                    <img className='login-page-logo' src={Logo} alt='App Logo' />
-                    <h1 className='login-page-h1'>apollo<span className='login-page-span'>fy</span></h1>
+                    <div className='login-page-heading'>
+                        <img className='login-page-logo' src={Logo} alt='App Logo' />
+                        <h1 className='login-page-h1'>apollo<span className='login-page-span'>fy</span></h1>
+                    </div>
                 </div>
             </div>
             <LoginButton />
